91898dc3cb424e61675f6f5b30a9778e85c682eb
Henrique Valcanaia <henriqueindalencio+github@gmail.com>
2020-11-06 18:58:06 -0300

    Setup new front target

---
+++ front/sources/FrontDefinitions.h
@@ -0,0 +1,8 @@
+//
+// Created by Henrique Valcanaia on 06/11/20.
+//
+
+#ifndef SISOP2_T1_FRONTDEFINITIONS_H
+#define SISOP2_T1_FRONTDEFINITIONS_H
+
+#endif //SISOP2_T1_FRONTDEFINITIONS_H
---
+++ front/sources/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Front end says: Hello darkness my old friend";
+    return EXIT_SUCCESS;
+}
