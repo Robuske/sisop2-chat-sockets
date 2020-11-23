b278f0ff6ca71588ae83bee5d4d2da0f22658045
Henrique Valcanaia <henriqueindalencio+github@gmail.com>
2020-11-23 19:38:02 -0300

    Parse server id argument

--- server/sources/main.cpp
+++ server/sources/main.cpp
@@ -2,34 +2,13 @@
 #include <iostream>
 #include <string>
 
+// ./server [load messages count] [id]
 int main(int argc, char *argv[]) {
+    std::cout << "Use um ID que esteja em ServerElectionManager.loadServerConfigFile()" << std::endl;
 
-//    if (argc != 2) {
-//      //  std::cout << "Usage: server <number of messages to load>" << std::endl;
-//      std::cout << "Invalid arguments number" << std::endl;
-//      exit(EXIT_FAILURE);
-//    }
-//
-//    int loadMessageCount = atoi(argv[1]);
-//    if (loadMessageCount < 0) {
-//        loadMessageCount = 0;
-//    }
-//
-//    int myID = atoi(argv[2]);
-//    if(myID < 0) {
-//        std::cout << "Invalid ID number. Server IDs must be a positive number" << std::endl;
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    int coordinatorID = atoi(argv[3]);
-//    if(coordinatorID < 0) {
-//        std::cout << "Invalid coordinator ID number. Server IDs must be a positive number" << std::endl;
-//        exit(EXIT_FAILURE);
-//    }
-//
-
-    if (argc != 2) {
-        std::cout << "Usage: server <number of messages to load>" << std::endl;
+    if (argc != 3) {
+        std::cout << "Usage: server [number of messages to load] [server id]" << std::endl;
+        std::cout << "Invalid arguments number" << std::endl;
         exit(EXIT_FAILURE);
     }
 
@@ -38,18 +17,20 @@ int main(int argc, char *argv[]) {
         loadMessageCount = 0;
     }
 
+    int myId = atoi(argv[2]);
+    if (myId <= 0) {
+        std::cout << "Invalid ID (" << myId << "). Server IDs must be a positive number" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
-    int serverID = 100;
     clearScreen();
 
-    std::cout << "Iniciando servidor Zap" << std::endl;
-    std::cout << "My ID: " << serverID << std::endl;
-    std::cout << "Iniciando servidor Zap" << std::endl;
+    std::cout << "Iniciando servidor Zap 2.0" << std::endl;
+    std::cout << "My ID: " << myId << std::endl;
     std::cout << "Vai conectar no front hardcoded: " << PORT_FRONT_SERVER << std::endl;
     std::cout << "Enviando as # últimas mensagens enviadas no grupo: " << std::to_string(loadMessageCount) << std::endl;
 
-
-    int startServerResult = ServerCommunicationManager().startServer(loadMessageCount, serverID);
+    int startServerResult = ServerCommunicationManager().startServer(loadMessageCount, myId);
     if (startServerResult < 0) {
         string errorPrefix = "Erro (" + std::to_string(startServerResult) + ") iniciando server";
         perror(errorPrefix.c_str());
